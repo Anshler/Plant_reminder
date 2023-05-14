62e34047659b808e84d05594019a4a637c08c135
Anshler <huynhminhtriet2002@gmail.com>
2023-05-14 13:19:44 +0700

    update

--- app_layout.py
+++ app_layout.py
@@ -16,6 +16,8 @@ Config.set('graphics', 'height', '700')
 
 # Declare Main pages ----------------------------------------
 class HomePage(Screen):
+    def test(self,instance):
+        print('home page')
     pass
 class PlantProfilePage(Screen):
     pass
@@ -30,26 +32,25 @@ class ShoppingPage(Screen):
 
 class MainPagesManager(ScreenManager):
     pass
-class UltilityBars(FloatLayout):
+class UtilityBars(FloatLayout):
     def open_setting(self, instance):
         print('setting')
-
     def home_buttons_animation(self, instance):
         if HomeButtons2Num[instance.name] != self.parent.Previous_home_buttons:
-            animate = Animation(pos_hint=instance.pos_hint, duration=0.25)
+            animate = Animation(pos_hint=instance.pos_hint, duration=0.1)
             animate.start(self.ids.home_highlight)
-            for a in range(len(self.parent.children)): # Can't call ids from parent (dun kno why), so iterate instead
-                if self.parent.children[a].name == 'main_pages':
-                    self.parent.children[a].transition.duration = 0.25
-                    if HomeButtons2Num[instance.name] > self.parent.Previous_home_buttons:
-                        self.parent.children[a].transition.direction = 'left'
-                    else:
-                        self.parent.children[a].transition.direction = 'right'
-                    self.parent.children[a].current = instance.name
-                    self.parent.Previous_home_buttons = HomeButtons2Num[instance.name]
-                    break
+            self.parent.ids.main_pages.transition.duration = 0.2
+            if HomeButtons2Num[instance.name] > self.parent.Previous_home_buttons:
+                self.parent.ids.main_pages.transition.direction = 'left'
+            else:
+                self.parent.ids.main_pages.transition.direction = 'right'
+            self.parent.ids.main_pages.current = instance.name
+            self.parent.Previous_home_buttons = HomeButtons2Num[instance.name]
+
 class MasterScreen(FloatLayout):
     Previous_home_buttons = 0
+    def swipe_home_buttons(self, instance):
+        print('swipe')
     pass
 
 kv = Builder.load_file('layout/MainLayout.kv')
