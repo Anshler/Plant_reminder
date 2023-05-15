ab9335f1c715b8789b0bff3cdc9a4ec209a10081
Anshler <huynhminhtriet2002@gmail.com>
2023-05-15 15:12:27 +0700

    update

--- app_layout.py
+++ app_layout.py
@@ -58,12 +58,15 @@ class MasterScreen(Screen):
     pass
 class LoginScreen(Screen):
     def __simple_validation(self,username,password):
-        time.sleep(1)
         if username == '' or password == '':
             print('can\'t login')
+            for a in range(0,50000000):
+                b=1
             return False
         try:
             # This would be an api call
+            for a in range(0, 50000000):
+                b = 1
             auth = [json.loads(a) for a in open('placeholder_server/user/user.json', encoding='utf-8')]
             for user in auth:
                 if user['username'].lower() == username and user['password'] == password:
@@ -71,16 +74,18 @@ class LoginScreen(Screen):
                 if user['gmail'].lower() == username and user['password'] == password:
                     return True
         except: pass
+        for a in range(0, 50000000):
+            b = 1
         print('can\'t login')
         return False
     def press_login_animation(self,instance): # Shrink button
-        animate = Animation(width=instance.width*0.95, height= instance.height*0.95,
+        animate = Animation(width=instance.width*0.95, height= instance.height*0.95, disabled = True,
                             center_x = instance.center_x, center_y = instance.center_y, duration=0.01)
         animate.start(self.ids.login_button_image)
         self.ids.login_button_image.source = 'layout/img/login_pressed.png'
     def login(self,instance): # Expand button and validate
         isUser = self.__simple_validation(self.ids.username.text.lower(),self.ids.password.text)
-        animate = Animation(width=instance.width, height=instance.height,
+        animate = Animation(width=instance.width, height=instance.height, disabled = False,
                             center_x=instance.center_x, center_y=instance.center_y, duration=0.01)
         animate.start(self.ids.login_button_image)
         self.ids.login_button_image.source = 'layout/img/login.png'
@@ -89,36 +94,41 @@ class LoginScreen(Screen):
             self.parent.transition.direction = 'up'
             self.parent.current = 'master_screen'
         else:
-            animate = Animation(color = (208/255, 0, 0,1),duration=0.2)+Animation(duration=1)+Animation(color = (0,0,0,0),duration=1)
+            animate = Animation(color = (208/255, 0, 0,1),duration=0.2)+Animation(duration=1)+Animation(color = (0,0,0,0),duration=0.5)
             animate.start(self.ids.wrong)
 
     def press_button(self,instance):
-        instance.color = (1,1,110/255,1)
-    def release_button(self, instance):
-        if instance.name == 'forget_password':
-            print('forget password')
-            instance.color = (124/255, 130/255, 161/255,1)
-        else:
-            print('signup')
-            instance.color = (51 / 255, 54 / 255, 71 / 255, 1)
-
-    def press_other_login(self,instance):
         animate = Animation(width=instance.width * 0.95, height=instance.height * 0.95,
                             center_x=instance.center_x, center_y=instance.center_y, duration=0.01)
         if instance.name == 'login_google':
             animate.start(self.ids.login_google_image)
-        else:
+        elif instance.name == 'login_facebook':
             animate.start(self.ids.login_facebook_image)
+        else:
+            instance.color = (1, 1, 110 / 255, 1)
+        instance.disabled = True
 
-    def release_other_login(self,instance):
-        animate = Animation(width=instance.width, height=instance.height,
-                            center_x=instance.center_x, center_y=instance.center_y, duration=0.01)
+
+    def release_button(self,instance):
+        animate = Animation(duration=0.5) + Animation(width=instance.width, height=instance.height,
+                                                      center_x=instance.center_x, center_y=instance.center_y,
+                                                      duration=0.01)
+        animate2 = Animation(duration=0.5)
         if instance.name == 'login_google':
-            print('login google')
             animate.start(self.ids.login_google_image)
-        else:
-            print('login facebook')
+            print('login google')
+        elif instance.name == 'login_facebook':
             animate.start(self.ids.login_facebook_image)
+            print('login facebook')
+        elif instance.name == 'forget_password':
+            animate2 += Animation(color=self.ids.welcome2.color, duration=0.1)
+            animate2.start(instance)
+            print('forget password')
+        else:
+            animate2 += Animation(color=self.ids.welcome.color, duration=0.1)
+            animate2.start(instance)
+            print('signup')
+        instance.disabled=False
 class WindowManager(ScreenManager):
     pass
 
