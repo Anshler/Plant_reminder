a5b34dcb3d03a89c8b8388bf4cb0eeaec594c506
Anshler <huynhminhtriet2002@gmail.com>
2023-05-16 02:45:54 +0700

    update

--- app_layout.py
+++ app_layout.py
@@ -69,9 +69,9 @@ class LoginScreen(Screen):
                 b = 1
             auth = [json.loads(a) for a in open('placeholder_server/user/user.json', encoding='utf-8')]
             for user in auth:
-                if user['username'].lower() == username and user['password'] == password:
+                if user['username'].lower() == username.lower() and user['password'] == password:
                     return True
-                if user['gmail'].lower() == username and user['password'] == password:
+                if user['email'].lower() == username.lower() and user['password'] == password:
                     return True
         except: pass
         for a in range(0, 50000000):
@@ -84,7 +84,9 @@ class LoginScreen(Screen):
         animate.start(self.ids.login_button_image)
         self.ids.login_button_image.source = 'layout/img/login_pressed.png'
     def login(self,instance): # Expand button and validate
+        # Make api call
         isUser = self.__simple_validation(self.ids.username.text.lower(),self.ids.password.text)
+        # request_function()
         animate = Animation(width=instance.width, height=instance.height, disabled = False,
                             center_x=instance.center_x, center_y=instance.center_y, duration=0.01)
         animate.start(self.ids.login_button_image)
@@ -143,6 +145,25 @@ class LoginScreen(Screen):
             change_color.start(instance)
             print('signup')
 class ForgetPasswordEmailScreen(Screen):
+    def __simple_validation(self,email):
+        if email == '':
+            print('Incorrect email')
+            for a in range(0,50000000):
+                b=1
+            return False
+        try:
+            # This would be an api call
+            for a in range(0, 50000000):
+                b = 1
+            auth = [json.loads(a) for a in open('placeholder_server/user/user.json', encoding='utf-8')]
+            for user in auth:
+                if user['email'].lower() == email.lower():
+                    return True
+        except: pass
+        for a in range(0, 50000000):
+            b = 1
+        print('Incorrect email')
+        return False
     def press_next_animation(self,instance): # Shrink button
         animate = Animation(width=instance.width*0.95, height= instance.height*0.95, disabled = True,
                             center_x = instance.center_x, center_y = instance.center_y, duration=0.01)
@@ -150,30 +171,84 @@ class ForgetPasswordEmailScreen(Screen):
         self.ids.next_forget_button_image.source = 'layout/img/login_pressed.png'
     def to_otp(self,instance):
         # Send the request to to server to notify
+        isUser = self.__simple_validation(self.ids.username_forget.text)
         # request_function()
         animate = Animation(width=instance.width, height=instance.height, disabled=False,
                             center_x=instance.center_x, center_y=instance.center_y, duration=0.01)
         animate.start(self.ids.next_forget_button_image)
         self.ids.next_forget_button_image.source = 'layout/img/login.png'
+        if isUser:
+            self.parent.transition.duration = 0.5
+            self.parent.transition.direction = 'left'
+            self.parent.current = 'forget_password_otp_screen'
+        else:
+            animate = Animation(color=MDApp.get_running_app().wrong_pass_warn,
+                                duration=0.2) + Animation(duration=1) + Animation(color=(0, 0, 0, 0), duration=0.5)
+            animate.start(self.ids.wrong_email)
+    def press_back(self,instance):
+        animate = Animation(width=instance.width * 0.95, height=instance.height * 0.95, disabled=True,
+                            center_x=instance.center_x, center_y=instance.center_y, duration=0.01)
+        animate.start(self.ids.back_button_image)
+    def release_back(self,instance):
+        animate = Animation(width=instance.width, height=instance.height, disabled=False,
+                            center_x=instance.center_x, center_y=instance.center_y, duration=0.01)
+        animate.start(self.ids.back_button_image)
+        closest_screen = self
+        while True:
+            if isinstance(closest_screen,Screen):
+                self.parent.transition.duration = 0.5
+                self.parent.transition.direction = 'right'
+                self.parent.current = 'login_screen'
+                break
+            else: closest_screen = closest_screen.parent
+
+class ForgetPasswordOTPScreen(Screen):
+    def press_confirm_animation(self,instance): # Shrink button
+        animate = Animation(width=instance.width*0.95, height= instance.height*0.95, disabled = True,
+                            center_x = instance.center_x, center_y = instance.center_y, duration=0.01)
+        animate.start(self.ids.confirm_otp_button_image)
+        self.ids.confirm_otp_button_image.source = 'layout/img/login_pressed.png'
+    def to_new_pass(self,instance):
+        # Send the request to to server to notify
+        # request_function()
+        animate = Animation(width=instance.width, height=instance.height, disabled=False,
+                            center_x=instance.center_x, center_y=instance.center_y, duration=0.01)
+        animate.start(self.ids.confirm_otp_button_image)
+        self.ids.confirm_otp_button_image.source = 'layout/img/login.png'
         self.parent.transition.duration = 0.5
         self.parent.transition.direction = 'left'
         self.parent.current = 'forget_password_new_pass_screen'
     def press_button(self,instance):
         change_size = Animation(disabled=True)
-        change_size.start(self.ids.remember_pass_back_image)
+        change_size.start(self.ids.resend_code_image)
         instance.color = MDApp.get_running_app().press_word_button
     def release_button(self,instance):
         change_size = Animation(disabled=False)
-        change_size.start(self.ids.remember_pass_back_image)
-        change_color = Animation(duration=0.5)+Animation(color=MDApp.get_running_app().primary_font_color, duration=0.1)
+        change_size.start(self.ids.resend_code_image)
+        change_color = Animation(duration=0.5) + Animation(color=MDApp.get_running_app().primary_font_color,
+                                                           duration=0.1)
         change_color.start(instance)
-        self.parent.transition.duration = 0.5
-        self.parent.transition.direction = 'right'
-        self.parent.current = 'login_screen'
+        # Make api call to resend code
+        # request_function()
+        print('resend otp')
+    def press_back(self,instance):
+        animate = Animation(width=instance.width * 0.95, height=instance.height * 0.95, disabled=True,
+                            center_x=instance.center_x, center_y=instance.center_y, duration=0.01)
+        animate.start(self.ids.back_button_image)
+    def release_back(self,instance):
+        animate = Animation(width=instance.width, height=instance.height, disabled=False,
+                            center_x=instance.center_x, center_y=instance.center_y, duration=0.01)
+        animate.start(self.ids.back_button_image)
+        closest_screen = self
+        while True:
+            if isinstance(closest_screen,Screen):
+                self.parent.transition.duration = 0.5
+                self.parent.transition.direction = 'right'
+                self.parent.current = 'forget_password_email_screen'
+                break
+            else: closest_screen = closest_screen.parent
 
 class ForgetPasswordNewPassScreen(Screen):
-    pass
-class ForgetPasswordOTPScreen(Screen):
     pass
 class SignUpScreen(Screen):
     pass
@@ -183,7 +258,7 @@ class WindowManager(ScreenManager):
     pass
 
 class PlantApp(MDApp):
-    previous_screen = [0]
+    previous_screen = [(None,0)]
     primary_font_color= 0,0,0,1
     secondary_font_color= 124 / 255, 130 / 255, 161 / 255, 1
     background_color = 1,1,1,1
