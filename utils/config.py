cd6e39099fbfcec04e666f9d1c4debdbc3618de0
Anshler <huynhminhtriet2002@gmail.com>
2023-05-16 16:47:56 +0700

    update

--- utils/config.py
+++ utils/config.py
@@ -5,10 +5,10 @@ except ImportError:
 import yaml
 
 meta_config = yaml.safe_load(open(resources.path('config','meta_config.yaml'),encoding='utf-8'))
-config = yaml.safe_load(open(resources.path('config','config_list.yaml'),encoding='utf-8'))
+theme = yaml.safe_load(open(resources.path('config','theme.yaml'),encoding='utf-8'))
 
-primary_font_color= config['theme'][meta_config['theme']]['primary_font_color']
-secondary_font_color= config['theme'][meta_config['theme']]['secondary_font_color']
-background_color = config['theme'][meta_config['theme']]['background_color']
-wrong_pass_warn = config['theme'][meta_config['theme']]['wrong_pass_warn']
-press_word_button = config['theme'][meta_config['theme']]['press_word_button']
+primary_font_color= theme[meta_config['theme']]['primary_font_color']
+secondary_font_color= theme[meta_config['theme']]['secondary_font_color']
+background_color = theme[meta_config['theme']]['background_color']
+wrong_pass_warn = theme[meta_config['theme']]['wrong_pass_warn']
+press_word_button = theme[meta_config['theme']]['press_word_button']
